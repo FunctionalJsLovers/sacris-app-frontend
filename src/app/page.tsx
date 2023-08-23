8f7ab4833f1d817a85e99af3ddcef27fe9ad3368
Miguel Rubiano <arkqngel343@gmail.com>
2023-08-22 21:34:25 -0500

    SAC-001 | Hola (#2)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Saludos cordiales al microfono de cesar&nbsp;
+          Saludos cordiales al microfono de cesar y a sichaca&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
