f21482458935dc63efa56033f6d1a3b50d974f04
cesar <cesar.moreno04@uptc.edu.co>
2023-08-30 17:12:50 -0500

    Create the navigation bar, about us 2

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -14,7 +14,7 @@ const AboutUs = () => {
                         <Image src="/images/Logo2.png" alt="Logo SacrisInk" width={200} height={200}></Image>
                     </div>
                     <div className="textAbout">
-                        <p>SacrisInk es un estudio de tatuajes con sede única en Tunja, Boyacá, el cual tiene como principal objetivo
+                        <p>SacrisInk. es un estudio de tatuajes con sede única en Tunja, Boyacá, el cual tiene como principal objetivo
                             incentivar el arte por medio del tatuaje. Este lugar cuenta con los insumos necesarios para llevar a cabo
                             este tipo de actividades de buena forma, y busca darle acogida a distintos artistas que quieran representar
                             su arte en los cuerpos de los habitantes de la ciudad.</p>
