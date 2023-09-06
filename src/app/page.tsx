b39d4d109556d8120cedf333f3e50ca6d2b28a9b
cesar <cesar.moreno04@uptc.edu.co>
2023-09-06 17:31:23 -0500

    SAC-8 | Create 'about us' page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import NavBar from "@/components/NavBar/Navbar";
+import AboutUs from "@/components/AboutUs/aboutUs";
 
 export default function Home() {
   return (
       <div id="all">
-        <NavBar/>
+        <AboutUs/>
       </div>
   )
 }
